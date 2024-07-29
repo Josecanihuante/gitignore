8a3a076cd8c925e7b80159acf87db5462c548628
Josecanihuante <60116953+Josecanihuante@users.noreply.github.com>
2024-07-29 15:06:24 -0400

    Delete Visita directory

--- Visita/Visita/build.sh
+++
@@ -1,8 +0,0 @@
-venv\bin\activate
-pip install --upgrade pip
-pip install -r requirements.txt
-refelx init
-reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
-deactivate
--- Visita/Visita/run.py
+++
@@ -1,4 +0,0 @@
-from Visita.Visita import app
-
-if __name__ == "__main__":
-    app._compile()
